b8176ec6e9505bdc36235e86bc16085b94812acc
Ivan Jelić <99324896+IvanJelicSF@users.noreply.github.com>
2023-06-21 12:19:17 +0200

    Update superdesk.config.js

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -39,13 +39,19 @@ module.exports = function(grunt) {
         },
 
         publisher: {
-            protocol: 'https',
-            tenant: process.env.PUBLISHER_API_SUBDOMAIN || '',
-            domain: process.env.PUBLISHER_API_DOMAIN || 'localhost',
-            base: 'api/v2',
-            wsDomain: process.env.PUBLISHER_WS_DOMAIN || process.env.PUBLISHER_API_DOMAIN,
-            wsPath: process.env.PUBLISHER_WS_PATH || '',
-            wsPort: process.env.PUBLISHER_WS_PORT || '8080'
+            protocol: 'https',                /* http or https */
+            tenant: '',              /* tenant - semantically subdomain, '' is allowed */
+            domain: 'sp-publisher.superdesk.pro',           /* domain name for the publisher */
+            base: 'api/v2',                  /* api base path */
+
+            wsProtocol: 'wss',                /* ws or wss (websocket); if unspecified or '' defaults to 'wss' */
+            wsDomain: 'sp-publisher.superdesk.pro',  /* domain name (usually domain as above) */
+                                            /* e.g.: example.com, abc.example.com */
+                                            /* tenant, as above, is NOT used for websocket */
+            wsPath: '/ws',                    /* path to websocket root dir */
+            wsPort: '80',                   /* if not specified: defaults to 443 for wss, 80 for ws */
+            hideContentRoutesInPublishPane: false, /* hides routes of type "content" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
+            hideCustomRoutesInPublishPane: false   /* hides routes of type "custom" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
         },
 
         langOverride: {
